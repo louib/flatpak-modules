ff68d4af151b9197c35c41f47f9421665c4f938a
louib <code@louib.net>
2022-06-23 13:29:36 -0400

    Add a stats script

---
+++ scripts/get_stats.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(realpath "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_DIR")
+
+die() { echo "$*" 1>&2 ; exit 1; }
+
+set -e
+
+APPS=""
+MODULES=""
+# TODO also count the sources?
+
+files=$(find "$SCRIPT_DIR/../")
+IFS=$'\n'; for file in $files; do
+    if [[ ! -f "$file" ]]; then
+        continue
+    fi
+    echo "$file"
+
+
+    manifest_type=$(fpcli get-type "$file" 2> /dev/null)
+    if [[ -z "$manifest_type" ]]; then
+        continue;
+    fi
+
+    if [[ "$manifest_type" == "application" ]]; then
+        APPS="$APPS\n$file"
+    fi
+
+    if [[ "$manifest_type" == "module" ]]; then
+        MODULES="$MODULES\n$file"
+    fi
+
+
+done
+unset IFS
+
+echo -e "Found applictions: $APPS"
+
+APP_COUNT=$(echo -e "$APPS" | wc -l)
+APP_COUNT=$(( APP_COUNT - 1 ))
+echo "Found $APP_COUNT application manifests."
+
+MODULE_COUNT=$(echo -e "$MODULES" | wc -l)
+MODULE_COUNT=$(( MODULE_COUNT - 1 ))
+echo "Found $MODULE_COUNT module manifests."
